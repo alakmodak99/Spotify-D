8b26eff68e88b2c20a98aa7c7f1639416ab68e31
Alak <alakmodak99@gmail.com>
2022-08-27 04:47:20 +0530

    Deploy

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Center from "../components/Center";
+import Sidebar from "../components/Sidebar";
+import { getSession } from "next-auth/react";
+import Player from "../components/Player";
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        {/* {Sidebar} */}
+        <Sidebar/>
+        <Center/>
+        {/* {Center} */}
+      </main>
+      <div className="sticky bottom-0">
+        {/* <Player/> */}
+        <Player/>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context)
+  return {
+    props:{
+      session,
+    },
+  };
+}
